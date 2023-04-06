1b398e96b139cea85636ccf7f74daa980d284501
OwlVi <mrowlvi@gmail.com>
2023-04-07 06:05:51 +0700

    elab 4-1

--- Elab/lab4-1.py
+++ Elab/lab4-1.py
@@ -1,12 +1,19 @@
 balance  = float(input("Enter Balance: "))
-deposit  = float(input("Enter Deposit: "))
-withdraw = float(input("Enter Withdraw: "))
 
-print("{:^55s}".format("-----------------------My Bank-----------------------"))
-print("{:^10s}|{:^14s}|{:^13s}|{:^13s}|".format("Date","WITHDRAW","DEPOSIT","BALANCE"))
-
-balance += deposit
-print("{:^10s}|{:-14.2f}|{:+13.2f}|{:-13.2f}|".format("15/03/23",0*(-1),deposit,balance))
-
-balance -= withdraw
-print("{:^10s}|{:-14.2f}|{:+13.2f}|{:-13.2f}|".format("",withdraw*(-1),0*(-1),balance))
+while (balance >= 0):
+    select = input(" Deposit (1) / Withdraw (2) / Exit (3) \n: ")
+    if select == '1' or select == "Deposit" or select == "deposit":
+        deposit  = float(input("Enter Deposit: "))
+        if deposit < 0 :
+            print("Cant not deposit")
+            break
+        balance += deposit
+    elif select == '2' or select == "Withdraw" or select == "withdraw":
+        withdraw = float(input("Enter Withdraw: "))
+        if withdraw < 0:
+            print("Cant not withdraw")
+            break
+        balance -= withdraw
+    else:
+        break
+print("My Balance: {:.2f}".format(balance))
